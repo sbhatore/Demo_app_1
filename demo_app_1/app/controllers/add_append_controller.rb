e7eaa31a6394ab8b2a10b371e3ad7a665e37f763
Siddharth Bhatore <sbhatore95@gmail.com>
2015-03-25 06:38:12 +0530

    improved indentation and coding style

--- demo_app_1/app/controllers/add_append_controller.rb
+++ demo_app_1/app/controllers/add_append_controller.rb
@@ -1,23 +1,22 @@
+# This controller has methods for add and append pages of the application
 class AddAppendController < ApplicationController
+  def home
+  end
 
-	def home
-
-	end
-
-	def append
-  end	
+  def append
+  end
 
   def add
   end
 
-	def view_appended
+  def view_appended
     if cookies.signed[:appended]
-      cookies.signed[:appended] = cookies.signed[:appended] << (params[:input].to_s)
+      cookies.signed[:appended] <<= (params[:input].to_s)
     else
       cookies.signed[:appended] = params[:input].to_s
     end
-     @appended = cookies.signed[:appended].to_i
- 	end
+    @appended = cookies.signed[:appended].to_i
+  end
 
   def view_added
     if cookies.signed[:added]
@@ -26,5 +25,5 @@ class AddAppendController < ApplicationController
       cookies.signed[:added] = params[:input].to_s
     end
     @added = cookies.signed[:added].to_i
-  end  
+  end
 end
